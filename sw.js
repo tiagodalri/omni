797dbdd724ce3392073716aaeac52af91624671d
Tiago Lourenco Dal Ri <tiagodalri1@live.com>
2026-09-17 13:58:21 -0300

    Corrige continuidade e precisao dos robos

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='omni-c5baefe3910e';
-const SHELL=["./","./index.html","./manifest.webmanifest","./pwa-omni/icon-192.png","./pwa-omni/icon-512.png","./assets/MonitoramentoPanel.js?v=c5baefe3910e","./assets/omni.css?v=c5baefe3910e","./assets/omni.js?v=c5baefe3910e"];
+const CACHE='omni-874bf31cc472';
+const SHELL=["./","./index.html","./manifest.webmanifest","./pwa-omni/icon-192.png","./pwa-omni/icon-512.png","./assets/MonitoramentoPanel.js?v=874bf31cc472","./assets/omni.css?v=874bf31cc472","./assets/omni.js?v=874bf31cc472"];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).catch(()=>{}));self.skipWaiting()});
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{const r=e.request;const u=new URL(r.url);if(r.method!=='GET'||u.origin!==self.location.origin||r.cache==='no-store'||u.searchParams.has('vivo'))return;
