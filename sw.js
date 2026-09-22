7598776110d65cc8d35a26fd3c449716f912b887
Tiago Lourenco Dal Ri <tiagodalri1@live.com>
2026-09-22 16:27:51 -0300

    OMNI: 2026-09-22 16:27

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='omni-78a01e3ed48d';
-const SHELL=["./","./index.html","./manifest.webmanifest","./pwa-omni/icon-192.png","./pwa-omni/icon-512.png","./assets/MonitoramentoPanel.js?v=78a01e3ed48d","./assets/omni.css?v=78a01e3ed48d","./assets/omni.js?v=78a01e3ed48d"];
+const CACHE='omni-e19cb265587c';
+const SHELL=["./","./index.html","./manifest.webmanifest","./pwa-omni/icon-192.png","./pwa-omni/icon-512.png","./assets/MonitoramentoPanel.js?v=e19cb265587c","./assets/omni.css?v=e19cb265587c","./assets/omni.js?v=e19cb265587c"];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).catch(()=>{}));self.skipWaiting()});
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{const r=e.request;const u=new URL(r.url);if(r.method!=='GET'||u.origin!==self.location.origin||r.cache==='no-store'||u.searchParams.has('vivo'))return;
